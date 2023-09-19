879237c38aebc7ca4e0bee1ce24c2cfdc187faf7
idrissjuanito <idrissjuanito@gmail.com>
2023-09-19 17:25:24 +0100

    task 10 amenties and places many to many rel sqlalchemy 5

--- models/amenity.py
+++ models/amenity.py
@@ -3,11 +3,11 @@
 from os import getenv
 
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
-    if getenv('HBNB_STORAGE_DB') == 'db':
+    if getenv('HBNB_TYPE_DB') == 'db':
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
         place_amenities = relationship('Place', secondary='place_amenity', viewonly=False,
--- models/place.py
+++ models/place.py
@@ -50,8 +50,8 @@ class Place(BaseModel, Base):
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
         reviews = relationship("Review", cascade="all, delete", backref="places")
-        # amenities = relationship("Amenity", secondary='place_amenity', viewonly=False,
-        #                          backref="place_amenities")
+        amenities = relationship("Amenity", secondary='place_amenity', viewonly=False,
+                                 backref="place_amenities")
     else:
         city_id = ""
         user_id = ""
