a4f9eb505f6d2a9beceed87cb36edf559fabd2ed
idrissjuanito <idrissjuanito@gmail.com>
2023-09-19 17:34:37 +0100

    task 10 amenties and places many to many rel sqlalchemy 2

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from os import getenv
-
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 class Amenity(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place', secondary='place_amenity', viewonly=False,
-                                       backref='amenities')
+        # place_amenities = relationship('Place', secondary='place_amenity', viewonly=False,
+        #                                backref='amenities')
     else:
         name = ''
