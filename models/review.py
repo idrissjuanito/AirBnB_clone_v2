afed35ebdaf4c57ade903460252935c5c019430e
idrissjuanito <idrissjuanito@gmail.com>
2023-09-19 16:26:30 +0100

    task 09 reviews table sqlalchemy orm: make files executable

--- models/review.py
+++ models/review.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from os import getenv
-from sqlalchemy import Column, ForeignKey, String
-from sqlalchemy.sql import Nullable
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, ForeignKey, String
 
 
 class Review(BaseModel, Base):
@@ -17,3 +16,7 @@ class Review(BaseModel, Base):
         place_id = ""
         user_id = ""
         text = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Place"""
+        super().__init__(*args, **kwargs)
