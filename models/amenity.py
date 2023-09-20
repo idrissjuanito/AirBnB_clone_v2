a4d5ab2a2ac1f16d0c13782e21937c5abfa0a89f
idrissjuanito <idrissjuanito@gmail.com>
2023-09-20 08:53:31 +0100

    relashionship in amenities table

--- models/amenity.py
+++ models/amenity.py
@@ -9,6 +9,6 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place', secondary='place_amenity', viewonly=False, back_populates='amenities')
+        # place_amenities = relationship('Place', secondary='place_amenity', viewonly=False, back_populates='amenities')
     else:
         name = ''
