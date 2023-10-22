3981c602fc1547afde083ce511a59ad58fd8a246
idrissjuanito <idrissjuanito@gmail.com>
2023-10-22 20:23:04 +0100

    basic flask route with variables

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+basic flask app with 3 routes
+one of the routes accepts a variable
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """
+    home route
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ simple route to return string """
+    return "HBNB"
+
+
+@app.route("/c/<param>", strict_slashes=False)
+def c_is_fun(param):
+    """
+    route display a string composed with
+    """
+    return f"C {param.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run()
