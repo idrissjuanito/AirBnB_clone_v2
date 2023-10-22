daa196ebca7db5eb5eef57419a1c9f6811a5c229
idrissjuanito <idrissjuanito@gmail.com>
2023-10-22 20:50:10 +0100

    flask route with param type converter

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+basic flask app with 5 routes
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """
+    home route
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ simple route to return string """
+    return "HBNB"
+
+
+@app.route("/c/<param>", strict_slashes=False)
+def c_is_fun(param):
+    """
+    route display a string composed with
+    """
+    return f"C {param.replace('_', ' ')}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+def pythons(text):
+    """
+    compose response string with option param
+    """
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def numbers(n):
+    """
+    response if param is integer
+    """
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run()
