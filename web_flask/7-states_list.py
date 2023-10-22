3a7eee817ff2cb1e7b1ba62abeba694bbe1bc8b7
idrissjuanito <idrissjuanito@gmail.com>
2023-10-22 23:45:05 +0100

    flask application with query from database

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+flask app for querying a list of states
+from airbnb project storage
+"""
+from models import storage, State
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def get_states():
+    """
+    queries storage for states and renders with jinja template
+    """
+    states = storage.all(State)
+    return render_template("7-states_list.html", states=states.items())
+
+
+def close_db(e=None):
+    """
+    used for cleanup by flask
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.teardown_appcontext(close_db)
+    app.run()
