f58e7a8d580bfa26bec1e1a165f08885e26e6e0c
idrissjuanito <idrissjuanito@gmail.com>
2023-10-22 21:19:01 +0100

    flask and a jinja template with conditionals

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+basic flask
+rendering jinja templates
+"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """
+    home route
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ simple route to return string """
+    return "HBNB"
+
+
+@app.route("/c/<param>", strict_slashes=False)
+def c_is_fun(param):
+    """
+    route display a string composed with
+    """
+    return f"C {param.replace('_', ' ')}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+def pythons(text):
+    """
+    compose response string with option param
+    """
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def numbers(n):
+    """
+    response if param is integer
+    """
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """
+    response if param is integer
+    """
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>")
+def odd_even(n):
+    """
+    renders jinja template which checks
+    passed param if even or odd
+    """
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run()
