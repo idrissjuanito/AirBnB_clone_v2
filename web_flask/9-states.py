31cf3a4e65274b2c4a9f84fd6a8e3736be1437db
idrissjuanito <idrissjuanito@gmail.com>
2023-10-23 03:11:55 +0100

    flask redering templates

---
+++ web_flask/9-states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+flask app for querying a list of cities by state
+from airbnb project storage
+"""
+from models import storage, State, City
+from flask import Flask, render_template
+
+from models.engine.db_storage import DBStorage
+app = Flask(__name__)
+store = DBStorage()
+store.reload()
+
+
+@app.teardown_appcontext
+def close_db(e=None):
+    """
+    used for cleanup by flask
+    """
+    storage.close()
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    """
+    renders a list of all states in jinja templates
+    """
+    states = store.all(State)
+    return render_template("7-states_list.html", states=states.items())
+
+
+@app.route("/states/<id>/", strict_slashes=False)
+def state_by_id(id):
+    """
+    renders a list of all states in jinja templates
+    """
+    states = store.all(State)
+    return render_template("9-states.html", states=states, id=f"State.{id}")
+
+
+if __name__ == "__main__":
+    app.run()
